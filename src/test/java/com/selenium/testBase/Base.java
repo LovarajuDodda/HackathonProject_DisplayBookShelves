4a504a68d24c0b5270ad68de1c53df27a69861f0
manasakancharla1 <126057913+manasakancharla1@users.noreply.github.com>
2026-02-11 09:36:24 +0530

    Update Base.java

--- src/test/java/com/selenium/testBase/Base.java
+++ src/test/java/com/selenium/testBase/Base.java
@@ -1,4 +1,24 @@
 package com.selenium.testBase;
 
-public class Base {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
+
