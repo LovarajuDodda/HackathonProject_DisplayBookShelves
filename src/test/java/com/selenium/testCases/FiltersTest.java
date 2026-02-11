0eb22a9f663ccdb44520da4f780d4255846863c1
manasakancharla1 <126057913+manasakancharla1@users.noreply.github.com>
2026-02-11 09:35:37 +0530

    Update FiltersTest.java

--- src/test/java/com/selenium/testCases/FiltersTest.java
+++ src/test/java/com/selenium/testCases/FiltersTest.java
@@ -1,4 +1,30 @@
 package com.selenium.testCases;
 
-public class FiltersTest {
+import com.selenium.pageObjects.HomePage;
+import com.selenium.pageObjects.FiltersPage;
+import com.selenium.testBase.BaseTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FiltersTest extends BaseTest {
+
+    @Test
+    public void testSearchAndApplyFilters() throws InterruptedException {
+
+        // Initialize page objects
+        HomePage homePage = new HomePage(driver);
+        FiltersPage filtersPage = new FiltersPage(driver);
+
+        // Test flow
+        homePage.openAmazon();
+        homePage.searchAmazon("open bookshelves");
+        filtersPage.setSliderToPrice(15000);
+        filtersPage.setOutOfStock();
+        filtersPage.displayFirstThreeItems();
+
+        // Quit driver at the end
+        driver.quit();
+    }
 }
+
