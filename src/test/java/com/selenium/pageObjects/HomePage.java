e463cad8f288b68982e8b69147dd271106d3df08
manasakancharla1 <126057913+manasakancharla1@users.noreply.github.com>
2026-02-11 09:32:36 +0530

    Update HomePage.java

--- src/test/java/com/selenium/pageObjects/HomePage.java
+++ src/test/java/com/selenium/pageObjects/HomePage.java
@@ -1,4 +1,30 @@
 package com.selenium.pageObjects;
 
-public class HomePage {
+import com.selenium.pageObjects.BasePage;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class HomePage extends BasePage {
+
+    @FindBy(id = "twotabsearchtextbox")
+    private WebElement searchBox;
+
+    @FindBy(id = "nav-search-submit-button")
+    private WebElement searchButton;
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void openAmazon() {
+        driver.get("https://www.amazon.in/");
+        // Ensure search box is present before continuing
+        fluentWait.until(ExpectedConditions.presenceOfElementLocated(By.id("twotabsearchtextbox")));
+    }
+
+    public void searchAmazon(String query) {
+        fluentWait.until(ExpectedConditions.visibilityOf(searchBox)).sendKeys(query);
+        fluentWait.until(ExpectedConditions.elementToBeClickable(searchButton)).click();
+    }
 }
